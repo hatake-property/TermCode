1b26c7fadb4bf7bb6a5d184493c50bf87ddb4de2
hatake <hermit183@outlook.com>
2025-05-14 11:44:21 +0900

    Color.Invert Method

--- Console.cs
+++ Console.cs
@@ -7,5 +7,12 @@ namespace TermCode
 			System.Console.BackgroundColor = System.ConsoleColor.White;
 			System.Console.ForegroundColor = System.ConsoleColor.Black;
 		}
+
+		public static void Invert()
+		{
+			System.ConsoleColor c = System.Console.BackgroundColor;
+			System.Console.BackgroundColor = System.Console.ForegroundColor;
+			System.Console.ForegroundColor = c;
+		}
 	}
 }
