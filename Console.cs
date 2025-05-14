1049c23b693998a5b1a3e9d142e28a44eab6ad8c
hatake <hermit183@outlook.com>
2025-05-14 11:37:56 +0900

    Color.Reset Method

---
+++ Console.cs
@@ -0,0 +1,11 @@
+namespace TermCode
+{
+	internal class Color
+	{
+		public static void Reset()
+		{
+			System.Console.BackgroundColor = System.ConsoleColor.White;
+			System.Console.ForegroundColor = System.ConsoleColor.Black;
+		}
+	}
+}
